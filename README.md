42cddfad973f2b2625ad2d6622dbb5efdbc73edc
rajeshb679 <rajeshb679@gmail.com>
2022-01-15 09:20:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-cbuzyi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-cbuzyi)
---
+++ arrayMethods.js
@@ -0,0 +1,60 @@
+//
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+const budget = [
+  { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
+  { value: -45, description: 'Groceries 🥑', user: 'jonas' },
+  { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
+  { value: 300, description: 'Freelancing 👩‍💻', user: 'jonas' },
+  { value: -1100, description: 'New iPhone 📱', user: 'jonas' },
+  { value: -20, description: 'Candy 🍭', user: 'matilda' },
+  { value: -125, description: 'Toys 🚂', user: 'matilda' },
+  { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
+];
+
+//Array push
+
+const length = budget.push({
+  value: -1900,
+  description: 'new toys',
+  user: 'jonas',
+});
+console.log(budget);
+console.log(length);
+
+dsfsdf;
